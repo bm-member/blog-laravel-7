e1d5eef1cf759e1a08292f5dbe6bddab77dcad24
Thet Minn Htun <thetminnhtun92@gmail.com>
2020-04-13 21:08:12 +0630

    Add Spatie Permissions Package

--- app/Category.php
+++ app/Category.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'description'];
     public function posts()
     {
         return $this->belongsToMany(Post::class)->withTimestamps();
